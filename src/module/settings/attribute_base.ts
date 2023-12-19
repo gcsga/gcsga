a091e6ddaafa6882ee9b03f85de15b54949ac37c
Mikolaj Tomczynski <mikolaj.tomczynski@digitalphobia.com>
2023-12-19 16:24:18 +0000

    added getData to abstract attribute-like menu class

--- src/module/settings/attribute_base.ts
+++ src/module/settings/attribute_base.ts
@@ -64,9 +64,9 @@ export abstract class AttributeBaseSettings extends SettingsMenuGURPS {
 	activateListeners(html: JQuery<HTMLElement>): void {
 		super.activateListeners(html)
 		// Html.find(".reset-all").on("click", event => this._onResetAll(event))
-		// html.find(".item").on("dragover", event => this._onDragItem(event))
+		html.find(".item").on("dragover", event => this._onDragItem(event))
 		html.find(".add").on("click", event => this._onAddItem(event))
-		// html.find(".delete").on("click", event => this._onDeleteItem(event))
+		html.find(".delete").on("click", event => this._onDeleteItem(event))
 	}
 
 
@@ -91,71 +91,128 @@ export abstract class AttributeBaseSettings extends SettingsMenuGURPS {
 	// }
 	//
 
-	async _onAddItem(event: JQuery.ClickEvent) {
-		event.preventDefault()
-		event.stopPropagation()
-		const type: string = $(event.currentTarget).data("type")
-		// HACK: idk what I'm doing
-		if ((<typeof AttributeBaseSettings> this.constructor).SETTINGS.includes(type as never)) {
-			const list: any[] = game.settings.get(SYSTEM_NAME, `${this.namespace}.${type}`) as any[]
-			list.push((<typeof AttributeBaseSettings> this.constructor).ObjectType[type])
-			await game.settings.set(SYSTEM_NAME, `${this.namespace}.${type}`, list)
-			return this.render
-		}
-		// if (this.constructor().SETTINGS)
-		// const attributes: any[] = game.settings.get(SYSTEM_NAME, `${SETTINGS.DEFAULT_ATTRIBUTES}.attributes`)
-		// const effects: any[] = game.settings.get(SYSTEM_NAME, `${SETTINGS.DEFAULT_ATTRIBUTES}.effects`)
-		// const type: ListType = $(event.currentTarget).data("type")
-		// let new_id = ""
-		// if (type === ListType.Attribute)
-		// 	for (let n = 0; n < 26; n++) {
-		// 		const char = String.fromCharCode(97 + n)
-		// 		if (![...attributes].find(e => e.id === char)) {
-		// 			new_id = char
-		// 			break
-		// 		}
-		// 	}
-		// switch (type) {
-		// 	case ListType.Attribute:
-		// 		// TODO: account for possibility of all letters being taken
-		// 		attributes.push({
-		// 			type: AttributeType.Integer,
-		// 			id: new_id,
-		// 			name: "",
-		// 			attribute_base: "",
-		// 			cost_per_point: 0,
-		// 			cost_adj_percent_per_sm: 0,
-		// 		})
-		// 		await game.settings.set(SYSTEM_NAME, `${SETTINGS.DEFAULT_ATTRIBUTES}.attributes`, attributes)
-		// 		return this.render()
-		// 	case ListType.Thresholds:
-		// 		attributes[$(event.currentTarget).data("id")].thresholds ??= []
-		// 		attributes[$(event.currentTarget).data("id")].thresholds!.push({
-		// 			state: "",
-		// 			explanation: "",
-		// 			expression: "",
-		// 			ops: [],
-		// 		})
-		// 		await game.settings.set(SYSTEM_NAME, `${SETTINGS.DEFAULT_ATTRIBUTES}.attributes`, attributes)
-		// 		return this.render()
-		// 	case ListType.Effect:
-		// 		effects.push({
-		// 			attribute: "",
-		// 			state: "",
-		// 			enter: [],
-		// 			leave: [],
-		// 		})
-		// 		await game.settings.set(SYSTEM_NAME, `${SETTINGS.DEFAULT_ATTRIBUTES}.effects`, effects)
-		// 		return this.render()
-		// 	case ListType.Enter:
-		// 	case ListType.Leave:
-		// 		effects[$(event.currentTarget).data("id")][type] ??= []
-		// 		effects[$(event.currentTarget).data("id")][type].push({
-		// 			id: ConditionID.Reeling,
-		// 			action: EFFECT_ACTION.ADD,
-		// 		})
-		// 		await game.settings.set(SYSTEM_NAME, `${SETTINGS.DEFAULT_ATTRIBUTES}.effects`, effects)
-		// 		return this.render()
-		// }
+	// abstract _onDragItem(event: JQuery.ClickEvent): Promise<unknown>
+	abstract _onAddItem(event: JQuery.ClickEvent): Promise<unknown>
+
+	abstract _onDeleteItem(event: JQuery.ClickEvent): Promise<unknown>
+
+
+	async _onDragStart(event: DragEvent) {
+		// TODO:update
+		const item = $(event.currentTarget!)
+		const type = item.data("type")
+		const index = Number(item.data("index"))
+		const parent_index = Number(item.data("pindex")) || 0
+		event.dataTransfer?.setData(
+			"text/plain",
+			JSON.stringify({
+				type: type,
+				index: index,
+				parent_index: parent_index,
+			})
+		)
+			; (event as any).dragType = type
 	}
+
+	protected _onDragItem(event: JQuery.DragOverEvent): void {
+		const element = $(event.currentTarget!)
+		const heightAcross = (event.pageY! - element.offset()!.top) / element.height()!
+		element.siblings(".item").removeClass("border-top").removeClass("border-bottom")
+		if (heightAcross > 0.5) {
+			element.removeClass("border-top")
+			element.addClass("border-bottom")
+		} else {
+			element.removeClass("border-bottom")
+			element.addClass("border-top")
+		}
+	}
+
+
+	override async getData(): Promise<any> {
+		// const attributes = game.settings.get(SYSTEM_NAME, `${SETTINGS.DEFAULT_ATTRIBUTES}.attributes`)
+		// const effects = game.settings.get(SYSTEM_NAME, `${SETTINGS.DEFAULT_ATTRIBUTES}.effects`)
+		// return {
+		// 	attributes: attributes,
+		// 	effects: effects,
+		// 	actor: null,
+		// 	config: CONFIG.GURPS,
+		// }
+		return (<typeof AttributeBaseSettings> this.constructor).SETTINGS.reduce((array, value) => (
+			{
+				...array,
+				[value]: game.settings.get(
+					SYSTEM_NAME, `${(<typeof AttributeBaseSettings> this.constructor).namespace}.${value}` as any
+				)
+			}), {
+			actor: null,
+			config: CONFIG.GURPS
+		}
+		)
+	}
+	// {
+	// event.preventDefault()
+	// event.stopPropagation()
+	// const type: string = $(event.currentTarget).data("type")
+	// // HACK: idk what I'm doing
+	// if ((<typeof AttributeBaseSettings> this.constructor).SETTINGS.includes(type as never)) {
+	// 	const list: any[] = game.settings.get(SYSTEM_NAME, `${this.namespace}.${type}`) as any[]
+	// 	list.push((<typeof AttributeBaseSettings> this.constructor).ObjectType[type])
+	// 	await game.settings.set(SYSTEM_NAME, `${this.namespace}.${type}`, list)
+	// 	return this.render()
+	// }
+	// if (this.constructor().SETTINGS)
+	// const attributes: any[] = game.settings.get(SYSTEM_NAME, `${SETTINGS.DEFAULT_ATTRIBUTES}.attributes`)
+	// const effects: any[] = game.settings.get(SYSTEM_NAME, `${SETTINGS.DEFAULT_ATTRIBUTES}.effects`)
+	// const type: ListType = $(event.currentTarget).data("type")
+	// let new_id = ""
+	// if (type === ListType.Attribute)
+	// 	for (let n = 0; n < 26; n++) {
+	// 		const char = String.fromCharCode(97 + n)
+	// 		if (![...attributes].find(e => e.id === char)) {
+	// 			new_id = char
+	// 			break
+	// 		}
+	// 	}
+	// switch (type) {
+	// 	case ListType.Attribute:
+	// 		// TODO: account for possibility of all letters being taken
+	// 		attributes.push({
+	// 			type: AttributeType.Integer,
+	// 			id: new_id,
+	// 			name: "",
+	// 			attribute_base: "",
+	// 			cost_per_point: 0,
+	// 			cost_adj_percent_per_sm: 0,
+	// 		})
+	// 		await game.settings.set(SYSTEM_NAME, `${SETTINGS.DEFAULT_ATTRIBUTES}.attributes`, attributes)
+	// 		return this.render()
+	// 	case ListType.Thresholds:
+	// 		attributes[$(event.currentTarget).data("id")].thresholds ??= []
+	// 		attributes[$(event.currentTarget).data("id")].thresholds!.push({
+	// 			state: "",
+	// 			explanation: "",
+	// 			expression: "",
+	// 			ops: [],
+	// 		})
+	// 		await game.settings.set(SYSTEM_NAME, `${SETTINGS.DEFAULT_ATTRIBUTES}.attributes`, attributes)
+	// 		return this.render()
+	// 	case ListType.Effect:
+	// 		effects.push({
+	// 			attribute: "",
+	// 			state: "",
+	// 			enter: [],
+	// 			leave: [],
+	// 		})
+	// 		await game.settings.set(SYSTEM_NAME, `${SETTINGS.DEFAULT_ATTRIBUTES}.effects`, effects)
+	// 		return this.render()
+	// 	case ListType.Enter:
+	// 	case ListType.Leave:
+	// 		effects[$(event.currentTarget).data("id")][type] ??= []
+	// 		effects[$(event.currentTarget).data("id")][type].push({
+	// 			id: ConditionID.Reeling,
+	// 			action: EFFECT_ACTION.ADD,
+	// 		})
+	// 		await game.settings.set(SYSTEM_NAME, `${SETTINGS.DEFAULT_ATTRIBUTES}.effects`, effects)
+	// 		return this.render()
+	// }
 }
